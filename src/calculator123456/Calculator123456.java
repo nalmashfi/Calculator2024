5fbfbc93b907d56dcd93e495c8604fe3c3f36069
nalma <nalma@Nabil>
2024-09-24 09:06:25 +0300

    Implemented add method

---
+++ src/calculator123456/Calculator123456.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator123456;
+
+/**
+ *
+ * @author nalma
+ */
+public class Calculator123456 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static int add(int x, int y) {
+        return x+y;
+    }
+    
+}
